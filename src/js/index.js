b09f8c347e3b8b3f6d2c755a8a16dd06abe88c6a
Michelle Barker <mbarker_84@hotmail.co.uk>
2021-03-21 22:57:52 +0000

    Update card url and loading class

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import menu from './menu'
 import scroll from './scroll'
 import textAnimation from './textAnimation'
 
+document.body.classList.remove('no-js')
+
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
     document.body.classList.add('is-complete')
